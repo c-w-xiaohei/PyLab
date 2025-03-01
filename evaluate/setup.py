bff3b02767935c54f43c9063260a011315a48486
c_w_xiaohei <1641233466@qq.com>
2025-03-01 15:37:59 +0800

    fix setup

--- evaluate/setup.py
+++ evaluate/setup.py
@@ -1,6 +1,12 @@
+import os
 from setuptools import setup
 from Cython.Build import cythonize
 
-setup(
-    ext_modules = cythonize("$module_name.py")
-)
+py_files = [f for f in os.listdir('.') if f.endswith('.py') and f != 'setup.py']
+
+if py_files:
+    setup(
+        ext_modules = cythonize(py_files)
+    )
+else:
+    print("没有找到需要编译的 Python 文件")
